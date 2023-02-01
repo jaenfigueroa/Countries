32008c29134d208277fc990dc8ac7871430f64f6
jaen <jaenfigueroa03042000@gmail.com>
2023-02-01 17:35:26 -0500

    contador de resultados del filtrado terminado

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -20,17 +20,22 @@ export const Selector = ({elemento, agregarEtiqueta, quitarEtiqueta}) => {
   // } 
 
   //MANEJAR LOS ELEMENTOS A MOSTRAR - FILTRADO 2 /////////////////
+
+  let [busquedaTexto, setBusquedaTexto] = useState(null)
+
   const filtrarOpciones = (evento)=>{
 
     //OBTENER LA BSUQUEDA ESCRITA EN EL BUSCADOR
     let busqueda = evento.target.value.toLowerCase()
+
+    setBusquedaTexto(evento.target.value.toLowerCase())
 
     //TRAER UN ARRAY DE LOS INPUT CHECKBOX
     // console.log(elemento.titulo)
     
     const resultados = document.querySelectorAll(`label`)
     
-    console.log(resultados);
+    // console.log(resultados);
     
     //FILTRAR LOS QUE NO COINCIDEN
 
@@ -48,21 +53,37 @@ export const Selector = ({elemento, agregarEtiqueta, quitarEtiqueta}) => {
 
     })
 
-
   } 
 
 
   //VERIFICAR EL CHECKBOX - AGREGAR O QUITAR ETIQUETA AL HACE CLICK EN EL CHECKBOX
+
+
   const verificarCheckbox = (evento, tipo) =>{
     // console.log(evento.target.checked);
     // console.log(evento.target.name) 
 
     if (evento.target.checked) {
+
       agregarEtiqueta(tipo, evento.target.name)
+
     } else{
       quitarEtiqueta(evento.target.name)
     }
   }
+
+
+  //EVALUAR LA CANTIDAD DE NDOOS QUE TIENE
+
+  const [numNodos, setNumNodos] = useState(elemento.opciones.length)
+
+  const evaluarCantidadResultados = (evento)=>{
+
+    let busqueda = evento.target.value.toLowerCase()
+    let resultados = elemento.opciones.filter(x => x.includes(busqueda))
+
+    setNumNodos(resultados.length)
+  } 
 
   /////////////////////////////////////////////////
   return (
@@ -81,9 +102,13 @@ export const Selector = ({elemento, agregarEtiqueta, quitarEtiqueta}) => {
             className='input'
             type="text"
             placeholder='Buscar'
-            onChange={filtrarOpciones} />
+            onChange={(evento)=>{
+              filtrarOpciones(evento)
+              // console.log('vaca');
+              evaluarCantidadResultados(evento)
+            }} />
 
-          <p className='numero'>{lista.length}</p>
+          <p className='numero'>{numNodos}</p>
 
           {/* CONTENEDOR DE OPCIONES */}
           <div className='contenedor-opciones' name={elemento.titulo}> {/* name: elemeento.titulo PROV */}
@@ -96,7 +121,14 @@ export const Selector = ({elemento, agregarEtiqueta, quitarEtiqueta}) => {
                 )
               ))
             }
+            
           </div>
+          
+          {
+            numNodos === 0 && (
+              <p className='aviso-resultados'>Lo sentimos, no encontramos resultados para <strong>"{busquedaTexto}"</strong>, intente buscar con un término más general.</p>
+            )
+          }
         </article>
 
     </div>
