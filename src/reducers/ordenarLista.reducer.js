2f54abc0435b076d2ab4a2dccfac0434dc769baa
jaen <jaenfigueroa03042000@gmail.com>
2023-02-02 16:08:13 -0500

    limpiado de codigo

--- src/reducers/ordenarLista.reducer.js
+++ src/reducers/ordenarLista.reducer.js
@@ -14,7 +14,7 @@ export const ordenarListaReducer = (estado, accion) => {
     case "MODIFICAR_LISTA":
       return { ...estado, listaOrdenada: [...payload] }
 
-    /* LOS FILTROS POR DEFCTO */
+    /* LOS FILTROS DE ORDEN */
     case 'NOMBRE_AZ':
       return {...estado, listaOrdenada: ordenarAZ(listaOrdenada)}
     case 'NOMBRE_ZA':
@@ -23,24 +23,21 @@ export const ordenarListaReducer = (estado, accion) => {
       return {...estado, listaOrdenada: ordenarAreaAscendente(listaOrdenada)}
     case 'AREA_DECENDENTE':
       return {...estado, listaOrdenada: ordenarAreaDecendente(listaOrdenada)}
-
-    /* LOS FILTROS AVANZADOS */
     case 'CONTINENTE_AZ':
       return {...estado, listaOrdenada: ordenarContinenteAZ(listaOrdenada)}
     case 'CONTINENTE_ZA':
       return {...estado, listaOrdenada: ordenarContinenteZA(listaOrdenada)}
-    
-
     case 'CAPITAL_AZ':
       return {...estado, listaOrdenada: ordenarCapitalAZ(listaOrdenada)}
     case 'CAPITAL_ZA':
       return {...estado, listaOrdenada: ordenarCapitalZA(listaOrdenada)}
-
     case 'MONEDA_AZ':
       return {...estado, listaOrdenada: ordenarMonedaAZ(listaOrdenada)}
     case 'MONEDA_ZA':
       return {...estado, listaOrdenada: ordenarMonedaZA(listaOrdenada)}
         
+    /* LOS FILTROS POR ETIQUETAS */
+
     default:
       return estado
   }
@@ -48,147 +45,79 @@ export const ordenarListaReducer = (estado, accion) => {
 
 //ORDENAR DE A-Z///////////////////////////
 const ordenarAZ = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.nombre < b.nombre) {
-      return -1
-    }
-    if (a.nombre > b.nombre) {
-      return 1
-    }
+    if (a.nombre < b.nombre) return -1
+    if (a.nombre > b.nombre) return 1
     return 0
   })
-
 }
 
 //ORDENAR DE Z-A///////////////////////////
 const ordenarZA = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.nombre > b.nombre) {
-      return -1
-    }
-    if (a.nombre < b.nombre) {
-      return 1
-    }
+    if (a.nombre > b.nombre) return -1
+    if (a.nombre < b.nombre) return 1
     return 0
   })
-
 }
 
 //ORDENAR POR AREA ASCENDENTE//////////////
 const ordenarAreaAscendente = (lista)=>{
-
-  // return lista.sort((a, b) => {
-  //   if (a.area < b.area) {
-  //     return -1
-  //   }
-  //   if (a.area > b.area) {
-  //     return 1
-  //   }
-  //   return 0
-  // })
-
   return lista.sort((a, b) => a.area - b.area)
-
 }
 
 //ORDERNAR POR AREA DESENDENTE//////////////
 const ordenarAreaDecendente = (lista)=>{
-
-  // return lista.sort((a, b) => {
-  //   if (a.area > b.area) {
-  //     return -1
-  //   }
-  //   if (a.area < b.area) {
-  //     return 1
-  //   }
-  //   return 0
-  // })
-
   return lista.sort((a, b) => b.area - a.area)
 }
 
 //ORDENAR POR CONTINENTE A-Z//////////////////////////////////////////
-
 const ordenarContinenteAZ = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.continente < b.continente) {
-      return -1
-    }
-    if (a.continente > b.continente) {
-      return 1
-    }
+    if (a.continente < b.continente) return -1
+    if (a.continente > b.continente) return 1
     return 0
   })
 }
 //ORDENAR POR CONTINENTE Z-A//////////////////////////////////////////
 const ordenarContinenteZA = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.continente > b.continente) {
-      return -1
-    }
-    if (a.continente < b.continente) {
-      return 1
-    }
+    if (a.continente > b.continente) return -1
+    if (a.continente < b.continente) return 1
     return 0
   })
 }
 
-
-
 //ORDENAR POR CAPITAL A-Z//////////////////////////////////////////
 const ordenarCapitalAZ = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.capital < b.capital) {
-      return -1
-    }
-    if (a.capital > b.capital) {
-      return 1
-    }
+    if (a.capital < b.capital) return -1
+    if (a.capital > b.capital)  return 1
     return 0
   })
 }
 //ORDENAR POR CAPITAL Z-A//////////////////////////////////////////
 const ordenarCapitalZA = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.capital > b.capital) {
-      return -1
-    }
-    if (a.capital < b.capital) {
-      return 1
-    }
+    if (a.capital > b.capital) return -1
+    if (a.capital < b.capital) return 1
     return 0
   })
 }
 
 //ORDENAR POR MONEDA A-Z//////////////////////////////////////////
 const ordenarMonedaAZ = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.moneda < b.moneda) {
-      return -1
-    }
-    if (a.moneda > b.moneda) {
-      return 1
-    }
+    if (a.moneda < b.moneda) return -1
+    if (a.moneda > b.moneda) return 1
     return 0
   })
 }
 //ORDENAR POR MONEDA Z-A//////////////////////////////////////////
 const ordenarMonedaZA = (lista)=>{
-
   return lista.sort((a, b) => {
-    if (a.moneda > b.moneda) {
-      return -1
-    }
-    if (a.moneda < b.moneda) {
-      return 1
-    }
+    if (a.moneda > b.moneda) return -1
+    if (a.moneda < b.moneda) return 1
     return 0
   })
 }
