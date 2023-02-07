f38210424a0cfa3d8eff2c9c82dc8a0c876b74cf
jaen <jaenfigueroa03042000@gmail.com>
2023-02-07 01:23:50 -0500

    terminado

--- src/pages/Individual/Carrucel.js
+++ src/pages/Individual/Carrucel.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 export const Carrucel = ({nombre, categoria}) => {
 
-  console.log(nombre)
+  // console.log(nombre)
 
   const [imagenes, setImagenes] = useState()
 
@@ -12,6 +12,8 @@ export const Carrucel = ({nombre, categoria}) => {
       const peticion = await fetch(`https://pixabay.com/api/?key=33435736-0bc5f29313ec2f94a8cd9b5cd&q=${nombre}&image_type=photo&pretty=true&lang=en&category=${categoria}`)
       const data = await peticion.json()
 
+      // console.log(data)
+
       let listaImagenes = data.hits.map(x => x = x.largeImageURL)
       setImagenes(listaImagenes)
     }
@@ -19,7 +21,7 @@ export const Carrucel = ({nombre, categoria}) => {
     traerImagenes()
   },[categoria, nombre])
 
-  console.log(nombre)
+  // console.log(nombre)
 
   if (imagenes && imagenes.length > 0) {
     return (
